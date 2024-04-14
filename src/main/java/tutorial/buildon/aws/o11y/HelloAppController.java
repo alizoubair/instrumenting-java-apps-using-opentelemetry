b459bdf0ab0d688c91e7d53a9f708f9b60f47be3
Ali Zoubair <ali.zoubair@outlook.com>
2024-04-14 20:47:58 +0100

    Added manual(whitebox) instrumentation

--- src/main/java/tutorial/buildon/aws/o11y/HelloAppController.java
+++ src/main/java/tutorial/buildon/aws/o11y/HelloAppController.java
@@ -2,8 +2,14 @@ package tutorial.buildon.aws.o11y;
 
 import java.util.Objects;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,15 +18,31 @@ public class HelloAppController {
     private static final Logger log =
         LoggerFactory.getLogger(HelloAppController.class);
 
+    @Value("otel.traces.api.version")
+    private String tracesApiVersion;
+
+    private final Tracer tracer =
+            GlobalOpenTelemetry.getTracer("io.opentelemetry.traces.hello",
+                    tracesApiVersion);
+
     @RequestMapping(method= RequestMethod.GET, value="/hello")
     public Response hello() {
         Response response = buildResponse();
+        Span span = tracer.spanBuilder("mySpan").startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            if (response.isValid()) {
+                log.info("The response is valid.");
+            }
+        } finally {
+            span.end();
+        }
         if (response.isValid()) {
             log.info("The response is valid.");
         }
         return response;
     }
 
+    @WithSpan
     private Response buildResponse() {
         return new Response("Hello World");
     }
